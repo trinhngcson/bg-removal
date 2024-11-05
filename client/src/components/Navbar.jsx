47a5e2834e3a2ccb25ffca5066ec143f7537517b
Trịnh Ngọc Sơn <77819889+trinhngcson@users.noreply.github.com>
2024-11-05 17:24:51 +0700

    buy credit + clerk signIn + create BE

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,28 @@
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 import { assets } from "../assets/assets";
 import { Link } from "react-router-dom";
 const Navbar = () => {
+  const { openSignIn } = useClerk();
+  const { isSignedIn, user } = useUser();
   return (
     <div className="sticky top-0 border-b-4 bg-white opacity-1 z-10">
       <div className="flex items-center justify-between mx-4 py-3 lg:mx-44">
         <Link to="/">
           <img className="w-32 sm:w-44" src={assets.logo} alt="" />
         </Link>
-        <button className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full ">
-          Get Started{" "}
-          <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
-        </button>
+        {isSignedIn ? (
+          <div>
+            <UserButton />
+          </div>
+        ) : (
+          <button
+            onClick={() => openSignIn({})}
+            className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full "
+          >
+            Get Started{" "}
+            <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
+          </button>
+        )}
       </div>
     </div>
   );
