3e0c966546be6eb41d9b1912cc29dd231423ca05
Trịnh Ngọc Sơn <77819889+trinhngcson@users.noreply.github.com>
2024-11-06 15:54:18 +0700

    webhooks added ( using svix)

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 //App config
 const PORT = process.env.PORT || 4000;
@@ -14,5 +15,6 @@ app.use(cors());
 
 //Api Rules
 app.get("/", (req, res) => res.send("API Working"));
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => console.log("Server Running on port " + PORT));
